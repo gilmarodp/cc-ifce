3712d3cc758beaf493c6afadb0d8050125b6c957
Gilmar Oliveira <42701487+gilmarodp@users.noreply.github.com>
2023-10-05 01:41:00 +0000

    removendo comentarios

--- S1/Fundamentos de Programação - Agebson/Lista 2/1.c
+++ S1/Fundamentos de Programação - Agebson/Lista 2/1.c
@@ -1,11 +1,3 @@
-// 1. Uma pessoa, ao realizar uma compra em um supermercado, recebe um número da sorte para concorrer a um
-// prêmio no final do mês. O cálculo do número da sorte é feito da seguinte forma:
-// • Caso a compra seja inferior a R$500,00 o número da sorte = 5000 - Valor a compra * Idade
-// • Caso a compra seja superior a R$500,00 o número da sorte = 50000 - Valor a compra * Idade
-// • Caso a compra seja igual a R$500,00 o número da sorte = Idade * Idade
-// Faça um programa em C que receba o Valor Total da Compra e o Ano de Nascimento e informe ao usuário
-// qual foi seu número da sorte.
-
 #include <stdio.h>
 
 int main()
