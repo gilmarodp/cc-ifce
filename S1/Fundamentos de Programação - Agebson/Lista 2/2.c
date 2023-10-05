74935578901ce6c3a29ade77d21179c091bb046b
Gilmar Oliveira <gilmar.odp@gmail.com>
2023-10-04 22:44:49 -0300

    questao 2

---
+++ S1/Fundamentos de Programação - Agebson/Lista 2/2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    float valorSalario = 0.0, valorFinanciamento = 0.0, valorSalarioConjuge = 0.0;
+    int financiamentoPorSalario = 0;
+
+    printf("Digite o valor do seu salário: ");
+    scanf("%f", &valorSalario);
+
+    printf("Digite o valor do financiamento: ");
+    scanf("%f", &valorFinanciamento);
+
+    financiamentoPorSalario = valorFinanciamento / valorSalario;
+
+    if (financiamentoPorSalario <= 5) {
+        printf("Financiamento concedido");
+    } else {
+        printf("Digite o valor do salário do seu cônjuge: ");
+        scanf("%f", &valorSalarioConjuge);
+
+        financiamentoPorSalario = valorFinanciamento / (valorSalario + valorSalarioConjuge);
+
+        if (financiamentoPorSalario <= 5) {
+            printf("Financiamento concedido");
+        } else {
+            printf("Financiamento Negado");
+        }
+    }
+    
+    printf("\nObrigado por nos consultar.\n");
+
+    return 0;
+}
